92314246c7a0af908b13c141c345a44a74c3b245
Christophe Schevers <christophe.schevers@gmail.com>
2023-12-08 18:34:06 +0100

    update

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import pyttsx3
 import PyPDF2
 
-with open('Test_PDF.pdf', 'rb') as book:
+with open('dummy.pdf', 'rb') as book:
     reader = PyPDF2.PdfReader(book)
 
-    audio_reader = pyttsx3.init()
-    audio_reader.setProperty("rate", 100)
+    engine = pyttsx3.init()
+    engine.setProperty("rate", 100)
 
     for page in range(len(reader.pages)):
         next_page = reader.pages[page]
         content = next_page.extract_text()
 
-        audio_reader.say(content)
-        audio_reader.runAndWait()
+        engine.say(content)
+        engine.runAndWait()
